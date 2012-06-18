3d26bf8271cb9a8c9ce7b7e0704d66a2abb82f82
Tue Ullerup Hansen <tueullerup@gmail.com>
2012-06-18 14:24:36 +0200

    Scripts

---
+++ ex15.rb
@@ -0,0 +1,15 @@
+filename = ARGV.first # Assigner første argument fra user-input til en variabel
+
+prompt = ">" # Laver bare krokodillenæbet når vi spørger user
+txt = File.open(filename) # Åbner filen og læser indholdet
+
+puts "Here's your file: #{filename}" # 
+puts txt.read()
+
+puts "I'll ask you to type it again:"
+print prompt
+file_again = gets.chomp()
+
+txt_again = File.open(file_again)
+
+puts txt_again.read()
